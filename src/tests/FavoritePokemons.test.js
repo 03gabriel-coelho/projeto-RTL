82fc494c8ba2a0726d4c19ee4a523701874f9306
Gabriel Coelho <coelho.tibbers@gmail.com>
2022-03-24 09:54:18 -0300

    Requisito 03 ok

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
 
 describe('Teste do componente FavoritePokemons', () => {
   it('É exibido na tela a mensagem "No favorite pokemon found"'
@@ -12,5 +15,14 @@ describe('Teste do componente FavoritePokemons', () => {
   });
 
   it('É exibido todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/pokemons/25');
+    const box = screen.getByRole('checkbox');
+    userEvent.click(box);
+
+    history.push('/favorites');
+    const NotFavorite = screen.getByText('More details');
+    expect(NotFavorite).toBeInTheDocument();
   });
 });
